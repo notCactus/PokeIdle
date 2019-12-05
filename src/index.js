5c4280456df1601f009a5a141cdc0e68fb480481
William <william.stackenas@bktv.nu>
2019-12-05 14:18:54 +0100

    Added redux skeleton

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
+import reducer from './reducers/reducers.js';
+import './index.css';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById("root")
   );
---
+++ src/reducers/reducers.js
@@ -0,0 +1,5 @@
+export default function reducer(state = {}, action){
+    return {
+
+    };
+}
