ba8f29f42c7c3518bb461f71325fbc54a14651e0
William <william.stackenas@bktv.nu>
2019-12-06 12:25:16 +0100

    Made sidebar implement react-redux

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -1,50 +1,18 @@
-import React from 'react';
-import SidebarLink from '../generalComponents/sidebarLink.js';
-import { useSelector, useDispatch } from 'react-redux';
-import setView from '../actions/actions';
-import './sidebar.css';
+import { connect } from 'react-redux'
+import SidebarPresentation from './sidebarPresentation';
 
-/*class Sidebar extends Component{
-    render(){
-        const view = useSelector(state => state.view);
-        return (
-            <div className="sidebar">
-                <img src={''} alt={'Logo'}/>
-                <div onClick={e => this.updateView(e.target, useDispatch())}>
-                    <Link to="/profile">PROFILE</Link>
-                    <Link to="/quest">QUEST</Link>
-                    <Link to="/shop">SHOP</Link>
-                </div>
-            </div>
-        );
+const mapDispatchToProps = (dispatch) => ({
+    setView: (view) => dispatch({
+        type: 'SET_VIEW',
+        view: view
+    })
+});
+
+const mapStateToProps = (state) => {
+    return {
+      view: state.view
     }
+};
 
-    updateView(target, dispatch) {
-        if (target.tagName === 'LINK'){
-            dispatch(setView(target.textContent));
-        }
-    }
-}*/
-
-function Sidebar() {
-    const view = useSelector(state => state.view);
-    const dispatch = useDispatch();
-    return (
-        <div className="sidebar">
-            <img src={''} alt={'Logo'}/>
-            <div className="sidebarNav" onClick={e => updateView(e.target, dispatch)}>
-                <SidebarLink to="/profile" value="PROFILE" view={view} className="sidebarLink"/>
-                <SidebarLink to="/quest" value="QUEST" view={view} className="sidebarLink"/>
-                <SidebarLink to="/shop" value="SHOP" view={view} className="sidebarLink"/>
-            </div>
-        </div>
-    );
-}
-
-function updateView(target, dispatch) {
-    if (target.tagName === 'SIDEBARLINK'){
-        dispatch(setView(target.value));
-    }
-}
-
+const Sidebar = connect(mapStateToProps, mapDispatchToProps)(SidebarPresentation);
 export default Sidebar;
