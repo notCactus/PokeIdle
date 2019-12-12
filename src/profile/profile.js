e7c797e455c5b25b2c085beedeae23070eebf760
Filip <filipgar@kth.se>
2019-12-12 13:28:53 +0100

    work on quets details

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import ProfileOverview from '../generalComponents/profileOverview/profileOverview';
 import RosterView from '../rosterView/rosterView';
 import './profile.css';
+import createProfileStore from '../stores/createProfileStore';
 
 class Profile extends Component{
   constructor(){
     super();
     this.state = {
-      image: "./loading.gif",
+      image:
+      `https://avatars.dicebear.com/v2/gridy/${createProfileStore.getState().username}.svg`,
     }
   }
   render(){
@@ -20,4 +22,5 @@ class Profile extends Component{
       </div>
     );
   }
+
 } export default Profile;
